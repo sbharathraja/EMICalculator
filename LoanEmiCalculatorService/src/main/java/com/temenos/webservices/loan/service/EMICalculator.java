2a9b2babd4f3d615b437fb845ee614eb5eac66ea
sbharathraja <sbharathraja@MAAD21101059.asia.temenosgroup.com>
2013-04-11 12:18:51 +0530

    Changes for Renamed repository

---
+++ LoanEmiCalculatorService/src/main/java/com/temenos/webservices/loan/service/EMICalculator.java
@@ -0,0 +1,34 @@
+package com.temenos.webservices.loan.service;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
+import javax.jws.WebService;
+
+import com.temenos.services.loan.core.DurationPeriods;
+import com.temenos.services.loan.core.EMIDetails;
+import com.temenos.services.loan.core.LoanCalculatorFactory;
+import com.temenos.services.loan.core.LoanEMICalculatorException;
+import com.temenos.services.loan.core.LoanType;
+
+/**
+ * Act as a Service Endpoint Implementation.
+ * 
+ * @author sbharathraja
+ * 
+ */
+@WebService(targetNamespace = "http://temenos.com")
+public class EMICalculator {
+
+	@WebMethod
+	@WebResult(name = "EMIDetails")
+	public EMIDetails calculateEmi(
+			@WebParam(name = "loanAmount") double loanAmount,
+			@WebParam(name = "loanType") LoanType loanType,
+			@WebParam(name = "durationPeriod") DurationPeriods durationPeriod)
+			throws LoanEMICalculatorException {
+		return LoanCalculatorFactory.createLoanCalculator().calculateEMI(
+				loanAmount, loanType, durationPeriod);
+	}
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-LoanCalculator
+This EMI Calculator project helps to extends the EMI Calculation as a SOAP based webservice. 
 ==============
